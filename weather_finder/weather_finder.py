c24aaa6c96dee668525fa862e236896d1d1f9781
Saad Yousaf <saad.yousaf@arbisoft.com>
2017-07-20 14:34:18 +0500

    following good coding practises, modifying weather_finder.py script

--- weather_finder/weather_finder.py
+++ weather_finder/weather_finder.py
@@ -3,47 +3,44 @@ import requests
 API_KEY = '9104e0a25f676b56ac31901d10c5cf0a'
 
 
+def get_weather_info(location):
+    """
+    Gets information about the weather of a location and displays it.
+
+    Arguments:
+        location (str): a string that specifies which place to search weather
+    """
+    location_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + API_KEY
+    weather_raw_data = requests.get(location_url)
+    weather_json = weather_raw_data.json()
+    if not verify_city(weather_json):
+        return
+    else:
+        weather_data_output_on_console(weather_data_organizer(weather_json))
+
+
 def verify_city(json_response):
     """
-    Takes in response from OWM and checks if city exists
+    Takes in response from OWM and checks if city exists.
 
     Arguments:
        json_response (json): contains response of a request from OWM
     """
-
     if json_response['cod'] == '404':
-        print 'City could not be found, please retry'
-	return False
-    else:
-	print 'City found'
-	return True
+        print('City could not be found, please retry')
+        return False
+
+    print('City found')
+    return True
 
 
-def get_weather_info(location):
+def weather_data_organizer(weather_raw_data):
     """
-    Gets information about the weather of a location and displays it
+    Gets information about the weather of a location and displays it.
 
     Arguments:
-        location (str): a string that specifies which place to search weather
+        weather_raw_data (json): a json object containing all data of location
     """
-	
-    location_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + API_KEY	
-    weather_raw_data = requests.get(location_url)
-    weather_json = weather_raw_data.json()
-    if not verify_city(weather_json):
-	return
-    else:
-	data_output(data_organizer(weather_json))
- 
-
-def data_organizer(weather_raw_data):
-    """
-    Gets information about the weather of a location and displays it
-
-    Arguments:
-        location (str): a string that specifies which place to search weather
-    """
-
     organized_data = dict(
         city = weather_raw_data.get('name'),
         country = weather_raw_data.get('sys').get('country'),
@@ -60,25 +57,24 @@ def data_organizer(weather_raw_data):
     return organized_data
 
 
-def data_output(data):
+def weather_data_output_on_console(weather_data):
     """
-    Prints all the important weather data of the provided place
+    Prints all the important weather data of the provided place.
 
     Arguments:
-        data (dict): a dictionary containing organized data of weather
+        weather_data (dict): a dictionary containing organized data of weather
     """
-    m_symbol = 'F'
+    degree_unit = 'F'
     print('---------------------------------------')
-    print('Current weather in: {}, {}:'.format(data['city'], data['country']))
-    print(data['temp'], m_symbol, data['sky'].encode('ascii'))
-    print('Max: {}, Min: {}'.format(data['temp_max'], data['temp_min']))
+    print('Current weather in: {}, {}:'.format(weather_data['city'], weather_data['country']))
+    print(weather_data['temp'], degree_unit, weather_data['sky'].encode('ascii'))
+    print('Max: {}, Min: {}'.format(weather_data['temp_max'], weather_data['temp_min']))
     print('')
-    print('Wind Speed: {}, Degree: {}'.format(data['wind'], data['wind_deg']))
-    print('Humidity: {}'.format(data['humidity']))
-    print('Cloud: {}'.format(data['cloudiness']))
-    print('Pressure: {}'.format(data['pressure']))
+    print('Wind Speed: {}, Degree: {}'.format(weather_data['wind'], weather_data['wind_deg']))
+    print('Humidity: {}'.format(weather_data['humidity']))
+    print('Cloud: {}'.format(weather_data['cloudiness']))
+    print('Pressure: {}'.format(weather_data['pressure']))
     print('---------------------------------------')
-
 
 
 location = 'London'
